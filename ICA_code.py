696dd3b1628979b82f710ac265c1de271bd91661
Aashrit Mathur <43023391+amathur4@users.noreply.github.com>
2019-12-08 13:32:34 -0600

    pushing my code

--- ICA_code.py
+++ ICA_code.py
@@ -1,16 +1,68 @@
+from sklearn.decomposition import FastICA
+from sklearn.preprocessing import Normalizer
+from matplotlib import pyplot as plt
+import numpy as np
+import pylab as pl
+import glob
+import cv2
+import os
+
 import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
-
-import cv2
-import numpy as np
 from PIL import Image, ImageTk
-from matplotlib import pyplot as plt
+
+def compute_ica_output(image_list, no_of_components, max_iterations, tolerance):
+    original_images = []
+    kspace_images = []
+    ica_images = []
+    kspace_output = []
+    for file in image_list:
+        image_name = file
+        input_image = cv2.imread(image_name, 0)
+        original_images.append(input_image)
+        input_image = np.fft.fft2(input_image)
+        input_image = np.fft.fftshift(input_image)
+        img = np.uint8(np.log(np.abs(input_image) + 1) * 10)
+        kspace_images.append(img)
+
+        img = np.fft.ifftshift(input_image)
+        img = np.fft.ifft2(img)
+        img = np.uint8(np.log(np.abs(img) + 1) * 10)
+
+        ica = FastICA(n_components = no_of_components, max_iter = max_iterations, tol = tolerance)
+        ica.fit(img)
+        image_ica = ica.fit_transform(img)
+        image_restored = ica.inverse_transform(image_ica)
+
+        imagefcs = np.zeros((image_restored.shape[0], image_restored.shape[1]), np.float)
+        for i in range(image_restored.shape[0]):
+            for j in range(image_restored.shape[1]):
+                imagefcs[i, j] = np.log(np.abs(image_restored[i, j]) * 10)
+        ica_images.append(imagefcs)
+
+        imagefcs = np.fft.fft2(imagefcs)
+        imagefcs = np.fft.fftshift(imagefcs)
+        output_kspace = np.uint8(np.log(np.abs(imagefcs) + 1) * 10)
+        kspace_output.append(output_kspace)
+    return original_images, kspace_images, ica_images, kspace_output
+
+input_image_list = ['P10-0001.jpg', 'P10-0002.jpg', 'P10-0003.jpg', 'P10-0004.jpg', 'P10-0005.jpg']
+original_images, kspace_images, ica_images, kspace_output = compute_ica_output(input_image_list, 15, 100000, 0.000001)
+
+plt.subplot(141),plt.imshow(original_images[1], cmap = 'gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(142),plt.imshow(kspace_images[1], cmap = 'gray')
+plt.title('K-space Input'), plt.xticks([]), plt.yticks([])
+plt.subplot(143),plt.imshow(ica_images[1], cmap = 'gray')
+plt.title('ICA Output'), plt.xticks([]), plt.yticks([])
+plt.subplot(144),plt.imshow(kspace_output[1], cmap = 'gray')
+plt.title('K-space Output'), plt.xticks([]), plt.yticks([])
+
+plt.show()
 
 window = tk.Tk() # window is the main window which pops up when the program runs
-
 class gui:
-
     # Input Image
 
     """def showImg(self, window):
